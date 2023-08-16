a02e8d3e9405bd56fa08344da110fe21b3fc1f61
KINGAVIP <avipruthi8@gmail.com>
2023-08-16 21:20:51 +0530

    Authentication app

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,44 @@
+'use client'
+import React from "react";
+import { useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import axios from 'axios'
+const Login = () => {
+    const router=useRouter()
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+
+    const handlesubmit=async(event)=>{
+        event.preventDefault();
+        try{
+            const res=axios.post('http://localhost:5000/api/auth/login',{email:email,password:password})
+            if((await res).status===200)
+            {
+              if((await res).data.success===true)
+                router.push('/')
+              else{
+                alert('Username or password is incorrect')
+              }
+            }
+            else{
+              router.push('/')
+            }
+        }catch(err)
+        {
+            console.log(err)
+        }
+        
+    }
+  return (
+    <div className="flex h-screen justify-center items-center bg-lime-100">
+      <form action="" method="post" className="flex flex-col  gap-5 p-10 bg-red-800 w-1/2 rounded-xl" onSubmit={handlesubmit}>
+        <h2 className="text-center text-white text-3xl font-bold">Login Page</h2>
+        <input type="text" name="email" value={email} className="py-3 px-4 rounded-full" onChange={(e)=>{setEmail(e.target.value)}} placeholder="Enter email" />
+        <input type="text" name="password" value={password} className="py-3 px-4 rounded-full" onChange={(e)=>{setPassword(e.target.value)}} placeholder="Enter password" />
+        <button className="bg-blue-200  p-3 hover:bg-blue-100 rounded-full text-xl " type="submit">Login here</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
